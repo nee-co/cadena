0134ca7c9879a017ce739f8de5ae7545a27c920e
Tomohiko Mimura <mito.5525@gmail.com>
2016-11-14 01:33:54 +0900

    ログイン中ユーザ取得処理を追加する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  before_action :authenticate_account!
+
+  def current_user
+    if @current_user.nil? && request.env["HTTP_X_CONSUMER_CUSTOM_ID"].present?
+      @current_user = Cuenta::User.find(request.env["HTTP_X_CONSUMER_CUSTOM_ID"])
+    end
+    @current_user
+  end
+
+  protected
+
+  def authenticate_account!
+    head :unauthorized and return unless current_user.present?
+  end
 end
